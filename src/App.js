712b3bec8f46419a0965a457eae89d07f9d141eb
accessmicro <vanlung99@gmail.com>
2021-04-12 23:11:29 +0700

    lan 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import App_Context from "./context/App_Context";
 import "./App.css";
 import React from "react";
-import App_InputRange from "./input-range/App_InputRange";
 function App() {
   return (
     <React.Fragment>
       <App_Context />
-      <App_InputRange/>
     </React.Fragment>
   );
 }
